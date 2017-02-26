f2feb50e0efc9e89e5887df3c12c41366ba14b84
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-02-26 14:35:15 -0500

    Created the UI

---
+++ mindReader/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  mindReader
+//
+//  Created by Sam Greenhill on 2/26/17.
+//  Copyright © 2017 simplyAmazingMachines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    
+    
+
+}
+
